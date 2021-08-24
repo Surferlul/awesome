6a8628bf507e87fef727e9236bce0fe6e7cad816
Surferlul <lukasabaumann@gmail.com>
2021-08-24 22:01:04 +0200

    added font installation

--- install.sh
+++ install.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 CACHE_DIR="/tmp/surferlul_awesome"
-[ -d "$CACHE_DIR" ] && rm -rf /tmp/surferlul_awesome
+[ -d "$CACHE_DIR" ] && echo "Removing previous cache" && rm -rf /tmp/surferlul_awesome
 mkdir $CACHE_DIR
+echo "Downloading AwesomeWM Configs"
 git clone https://github.com/Surferlul/awesome.git $CACHE_DIR
-[ -d "$HOME/.config/awesome" ] && tar cf $CACHE_DIR/previous.tar $HOME/.config/awesome/ && rm -rf $HOME/.config/awesome/
+[ -d "$HOME/.config/awesome" ] && echo "Backing up previous configs" && tar cf $CACHE_DIR/previous.tar $HOME/.config/awesome/ && echo "Removing previous configs" && rm -rf $HOME/.config/awesome/
+echo "Downloading Vicious widgets for AwesomeWM"
 git clone https://github.com/vicious-widgets/vicious.git $CACHE_DIR/vicious
+echo "Moving configs to install location"
 mv $CACHE_DIR $HOME/.config/awesome
 [ "$1" != "--preserve-git" ] && rm -rf $HOME/.config/awesome/.git
+[ ! -d "$HOME/.fonts" ] && echo "Creating ~/.fonts directory" && mkdir $HOME/.fonts
+[ -z "$(ls $HOME/.fonts | grep "[Ii]ndie *[Ff]lower")" ] && echo "Indie Flower doesn't exist, installing" && mkdir $CACHE_DIR && curl 'https://google-webfonts-helper.herokuapp.com/api/fonts/indie-flower?download=zip&subsets=latin&variants=regular' > $CACHE_DIR/IndieFlower.zip && 7z x $CACHE_DIR/IndieFlower.zip && cp $CACHE_DIR/*.ttf $HOME/.fonts && rm -rf $CACHE_DIR
