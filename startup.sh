85c77a3e5ec2f028706cc89f1651be2bfd99f9e5
Surferlul <lukasabaumann@gmail.com>
2021-08-24 19:06:46 +0200

    changed startup

--- startup.sh
+++ startup.sh
@@ -5,3 +5,4 @@ xinput --set-prop "SYNA7DB5:01 06CB:CD41 Touchpad" $(xinput list-props $(xinput 
 xinput --set-prop "SYNA7DB5:01 06CB:CD41 Touchpad" $(xinput list-props $(xinput list | grep 'Touchpad' | cut -f 2 | cut -d'=' -f 2) | grep "Middle Emulation Enabled (" | cut -d'(' -f 2 | cut -d')' -f 1) 1
 #python ~/user/projects/startup/presence.py &
 picom --experimental-backends --backend glx &
+cp ~/.gtkrc-2.0_default ~/.gtkrc-2.0
